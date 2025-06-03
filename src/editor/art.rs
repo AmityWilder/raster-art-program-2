4310673e3443b944cca2c284d90b1b6ba273f004
Amy Wilder <74995093+HenryWilder@users.noreply.github.com>
2025-06-03 19:38:02 -0400

    Add flood fill

--- src/editor/art.rs
+++ src/editor/art.rs
@@ -1,12 +1,118 @@
+use std::collections::VecDeque;
 use raylib::prelude::*;
 use crate::{brush::Brush, draw_texture_custom, frame::Frame};
+
+fn get_pixel(img: &mut Image, x: i32, y: i32) -> Option<Color> {
+    if 0 <= x && x < img.width && 0 <= y && y < img.height {
+        Some(img.get_color(x, y))
+    } else {
+        None
+    }
+}
+
+/// ```not_rust
+/// fn fill(x, y):
+///     if not Inside(x, y) then return
+///     let s = new empty queue or stack
+///     Add (x, x, y, 1) to s
+///     Add (x, x, y - 1, -1) to s
+///     while s is not empty:
+///         Remove an (x1, x2, y, dy) from s
+///         let x = x1
+///         if Inside(x, y):
+///             while Inside(x - 1, y):
+///                 Set(x - 1, y)
+///                 x = x - 1
+///             if x < x1:
+///                 Add (x, x1 - 1, y - dy, -dy) to s
+///         while x1 <= x2:
+///             while Inside(x1, y):
+///                 Set(x1, y)
+///                 x1 = x1 + 1
+///             if x1 > x:
+///                 Add (x, x1 - 1, y + dy, dy) to s
+///             if x1 - 1 > x2:
+///                 Add (x2 + 1, x1 - 1, y - dy, -dy) to s
+///             x1 = x1 + 1
+///             while x1 < x2 and not Inside(x1, y):
+///                 x1 = x1 + 1
+///             x = x1
+/// ```
+fn flood_fill(img: &mut Image, x: i32, y: i32, new_color: Color) {
+    // if not Inside(x, y) then return
+    let Some(old_color) = get_pixel(img, x, y) else { return };
+    if new_color == old_color { return; } // already filled
+    // let s = new empty queue or stack
+    let mut s = VecDeque::new();
+    // Add (x, x, y, 1) to s
+    s.push_back((x, x, y, 1));
+    // Add (x, x, y - 1, -1) to s
+    s.push_back((x, x, y - 1, -1));
+    // while s is not empty:
+    //     Remove an (x1, x2, y, dy) from s
+    while let Some((mut x1, x2, y, dy)) = s.pop_front() {
+        // let x = x1
+        let mut x = x1;
+        // if Inside(x, y):
+        if get_pixel(img, x, y).is_some_and(|c| c == old_color) {
+            // while Inside(x - 1, y):
+            while get_pixel(img, x - 1, y).is_some_and(|c| c == old_color) {
+                // Set(x - 1, y)
+                img.draw_pixel(x - 1, y, new_color);
+                // x = x - 1
+                x -= 1;
+            }
+            // if x < x1:
+            if x < x1 {
+                // Add (x, x1 - 1, y - dy, -dy) to s
+                s.push_back((x, x1 - 1, y - dy, -dy));
+            }
+        }
+        // while x1 <= x2:
+        while x1 <= x2 {
+            // while Inside(x1, y):
+            while get_pixel(img, x1, y).is_some_and(|c| c == old_color) {
+                // Set(x1, y)
+                img.draw_pixel(x1, y, new_color);
+                // x1 = x1 + 1
+                x1 += 1;
+            }
+            // if x1 > x:
+            if x1 > x {
+                // Add (x, x1 - 1, y + dy, dy) to s
+                s.push_back((x, x1 - 1, y + dy, dy));
+            }
+            // if x1 - 1 > x2:
+            if x1 - 1 > x2 {
+                // Add (x2 + 1, x1 - 1, y - dy, -dy) to s
+                s.push_back((x2 + 1, x1 - 1, y - dy, -dy));
+            }
+            // x1 = x1 + 1
+            x1 += 1;
+            // while x1 < x2 and not Inside(x1, y):
+            while x1 < x2 && !get_pixel(img, x1, y).is_some_and(|c| c == old_color) {
+                // x1 = x1 + 1
+                x1 += 1;
+            }
+            // x = x1
+            x = x1;
+        }
+    }
+}
+
+enum Tool {
+    Pen {
+        pen_pos_prev: Option<(Vector2, Option<Vector2>)>,
+    },
+    Fill,
+}
 
 pub struct ArtEditor {
     canvas: RenderTexture2D,
     is_canvas_dirty: bool,
-    pen_pos_prev: Option<(Vector2, Option<Vector2>)>,
     zoom_pow: f32,
     frac_pan: Vector2,
+    tool: Tool,
 }
 
 impl ArtEditor {
@@ -14,9 +120,11 @@ impl ArtEditor {
         Self {
             canvas,
             is_canvas_dirty: true,
-            pen_pos_prev: None,
             zoom_pow: 0.0,
             frac_pan: Vector2::zero(),
+            tool: Tool::Pen {
+                pen_pos_prev: None,
+            },
         }
     }
 
@@ -82,29 +190,50 @@ impl ArtEditor {
             y: (mouse_pos.y*zoom_inv - self.frac_pan.y).round(),
         };
 
+        if rl.is_key_pressed(KeyboardKey::KEY_G) {
+            self.tool = Tool::Fill;
+        } else if rl.is_key_pressed(KeyboardKey::KEY_B) {
+            if !matches!(self.tool, Tool::Pen { .. }) {
+                self.tool = Tool::Pen { pen_pos_prev: None };
+            }
+        }
+
         // Paint
-        {
-            if  rl.is_mouse_button_released(MouseButton::MOUSE_BUTTON_LEFT) ||
-                rl.is_mouse_button_released(MouseButton::MOUSE_BUTTON_RIGHT)
-            {
-                self.pen_pos_prev = None;
-            }
+        match &mut self.tool {
+            Tool::Pen { pen_pos_prev } => {
+                if  rl.is_mouse_button_released(MouseButton::MOUSE_BUTTON_LEFT) ||
+                    rl.is_mouse_button_released(MouseButton::MOUSE_BUTTON_RIGHT)
+                {
+                    *pen_pos_prev = None;
+                }
 
-            if let Some((pos_prev, pos_pprev)) = &mut self.pen_pos_prev && pos_prev != &pen_pos {
-                let mut d = rl.begin_texture_mode(&thread, &mut self.canvas);
-                let pos_pprev = std::mem::replace(pos_pprev, Some(*pos_prev));
-                let pos_prev = std::mem::replace(pos_prev, pen_pos);
-                brush.paint(&mut d, Some((pos_prev, pos_pprev)), pen_pos);
-                self.is_canvas_dirty = true;
-            }
-
-            if self.pen_pos_prev.is_none() {
-                let is_right_pressed = rl.is_mouse_button_pressed(MouseButton::MOUSE_BUTTON_RIGHT);
-                if rl.is_mouse_button_pressed(MouseButton::MOUSE_BUTTON_LEFT) || is_right_pressed {
-                    self.pen_pos_prev = Some((pen_pos, None));
-                    brush.is_erasing = is_right_pressed;
+                if let Some((pos_prev, pos_pprev)) = &mut *pen_pos_prev && pos_prev != &pen_pos {
                     let mut d = rl.begin_texture_mode(&thread, &mut self.canvas);
-                    brush.paint(&mut d, None, pen_pos);
+                    let pos_pprev = std::mem::replace(pos_pprev, Some(*pos_prev));
+                    let pos_prev = std::mem::replace(pos_prev, pen_pos);
+                    brush.paint(&mut d, Some((pos_prev, pos_pprev)), pen_pos);
+                    self.is_canvas_dirty = true;
+                }
+
+                if pen_pos_prev.is_none() {
+                    let is_right_pressed = rl.is_mouse_button_pressed(MouseButton::MOUSE_BUTTON_RIGHT);
+                    if rl.is_mouse_button_pressed(MouseButton::MOUSE_BUTTON_LEFT) || is_right_pressed {
+                        *pen_pos_prev = Some((pen_pos, None));
+                        brush.is_erasing = is_right_pressed;
+                        let mut d = rl.begin_texture_mode(&thread, &mut self.canvas);
+                        brush.paint(&mut d, None, pen_pos);
+                        self.is_canvas_dirty = true;
+                    }
+                }
+            }
+
+            Tool::Fill => {
+                if rl.is_mouse_button_pressed(MouseButton::MOUSE_BUTTON_LEFT) {
+                    let mut img = self.canvas.load_image().unwrap();
+                    flood_fill(&mut img, pen_pos.x as i32, pen_pos.y as i32, brush.color);
+                    let len = get_pixel_data_size(img.width, img.height, img.format()).try_into().unwrap();
+                    let pixels = unsafe { std::slice::from_raw_parts(img.data.cast(), len) };
+                    self.canvas.update_texture(pixels).unwrap();
                     self.is_canvas_dirty = true;
                 }
             }
